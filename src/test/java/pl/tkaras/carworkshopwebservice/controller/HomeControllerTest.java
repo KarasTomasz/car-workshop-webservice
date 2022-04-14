c8d13039fb0b72d308ad05e8bc0e0f9cade875cf
KarasTomasz <t.karas@onet.pl>
2022-04-15 01:20:36 +0200

    Add homepage tests

---
+++ src/test/java/pl/tkaras/carworkshopwebservice/controller/HomeControllerTest.java
@@ -0,0 +1,30 @@
+package pl.tkaras.carworkshopwebservice.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void homePageTest() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("home"));
+
+
+    }
+}
