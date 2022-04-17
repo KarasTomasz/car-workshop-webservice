6dfc1ff844cde1003c9d1e78d48bce7f02f19e57
KarasTomasz <t.karas@onet.pl>
2022-04-17 20:36:26 +0200

    inject CarDtoMapper, update all methods

--- src/main/java/pl/tkaras/carworkshopwebservice/logic/CarService.java
+++ src/main/java/pl/tkaras/carworkshopwebservice/logic/CarService.java
@@ -1,51 +1,84 @@
 package pl.tkaras.carworkshopwebservice.logic;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import pl.tkaras.carworkshopwebservice.model.dto.CarDto;
+import pl.tkaras.carworkshopwebservice.model.entity.AppUser;
 import pl.tkaras.carworkshopwebservice.model.mapper.impl.CarDtoMapper;
 import pl.tkaras.carworkshopwebservice.model.entity.Car;
 import pl.tkaras.carworkshopwebservice.repository.CarRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CarService {
 
-    private CarRepository carRepo;
-    private CarDtoMapper carDtoMapper;
+    private final CarRepository carRepo;
+    private final CarDtoMapper carDtoMapper;
 
-    private CarService(CarRepository carRepo) {
+    public CarService(CarRepository carRepo, CarDtoMapper carDtoMapper) {
         this.carRepo = carRepo;
+        this.carDtoMapper = carDtoMapper;
     }
 
-    public CarDto getSingleCar(Long id){
-        if(carRepo.existsById(id)){
-            return new CarDtoMapper().mapToDto(carRepo.findById(id)
-                    .orElseThrow(() -> new IllegalStateException(String.format("Problem with given id %s", id))));
-        }
-        throw new UsernameNotFoundException(String.format("User with id %s not found", id));
+    public Optional<CarDto> getCar(Long id){
+        Car car = carRepo.findById(id)
+                .orElseThrow(() -> new IllegalStateException(String.format("id %s not found", id)));
+        return Optional.ofNullable( new CarDtoMapper().mapToDto(car));
     }
 
     public List<CarDto> gelAllCars(){
         return carDtoMapper.mapToDtos(carRepo.findAll());
     }
 
-    public ResponseEntity addCar(Car entity){
-        if(carRepo.existsById(entity.getId())){
-            carDtoMapper.mapToDto(carRepo.save(entity));
-            return ResponseEntity.ok().build();
-        }
-        return ResponseEntity.notFound().build();
+    public List<CarDto> gelAllCarsByUsername(String username){
+        AppUser appUser = carRepo.findByUsername(username)
+                .orElseThrow(() -> new IllegalStateException(String.format("user %s not found", username)));
+
+        return carDtoMapper.mapToDtos(carRepo.findAllByAppUserId(appUser.getId()));
     }
 
-    public ResponseEntity deleteCar(Long id){
+    public CarDto addCar(CarDto cardto){
+            Car car = new Car().builder()
+                    .createdOn(LocalDateTime.now())
+                    .mark(cardto.getMark())
+                    .model(cardto.getModel())
+                    .description(cardto.getDescription())
+                    .appUser(carRepo.findByUsername(cardto.getUsername())
+                            .orElseThrow(() -> new IllegalStateException(String.format("User %s not found", cardto.getUsername()))))
+                    .build();
+
+        return carDtoMapper.mapToDto(carRepo.save(car));
+    }
+
+    public CarDto updateCar(Long id, CarDto cardto){
+        if(carRepo.existsById(id)){
+            Car car = carRepo.findById(id)
+                    .orElseThrow(() -> new IllegalStateException(String.format("id %s not found", id)));
+
+            car.setModel(cardto.getMark());
+            car.setModel(cardto.getModel());
+            car.setDescription(cardto.getDescription());
+            car.setCreatedOn(car.getCreatedOn());
+            car.setUpdatedOn(LocalDateTime.now());
+            car.setAppUser(car.getAppUser());
+
+            carRepo.save(car);
+            return carDtoMapper.mapToDto(car);
+        }
+        else {
+            throw new IllegalStateException(String.format("id %s not found", id));
+        }
+    }
+
+    public void deleteCar(Long id){
         if(carRepo.existsById(id)){
             carRepo.deleteById(id);
-            return ResponseEntity.ok().build();
         }
-        return ResponseEntity.notFound().build();
+        else{
+            throw new IllegalStateException(String.format("id %s not found", id));
+        }
     }
 
 }
