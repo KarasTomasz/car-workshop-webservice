bee1eb5ecdfff3187787abb7310f28141982568f
KarasTomasz <t.karas@onet.pl>
2022-04-17 21:00:40 +0200

    add exception

---
+++ src/main/java/pl/tkaras/carworkshopwebservice/exception/AppUserNotFoundException.java
@@ -0,0 +1,12 @@
+package pl.tkaras.carworkshopwebservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class AppUserNotFoundException extends RuntimeException {
+
+    public AppUserNotFoundException(String username) {
+        super(String.format("user %s not found", username));
+    }
+}
