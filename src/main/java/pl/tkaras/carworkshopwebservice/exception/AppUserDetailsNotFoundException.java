c31cb24cf520763a56ea69d8005bfe80b9a3d172
KarasTomasz <t.karas@onet.pl>
2022-04-18 21:15:21 +0200

    add exception

---
+++ src/main/java/pl/tkaras/carworkshopwebservice/exception/AppUserDetailsNotFoundException.java
@@ -0,0 +1,12 @@
+package pl.tkaras.carworkshopwebservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class AppUserDetailsNotFoundException extends RuntimeException {
+
+    public AppUserDetailsNotFoundException(String username) {
+        super(String.format("role and expiration information for user %s not found", username));
+    }
+}
