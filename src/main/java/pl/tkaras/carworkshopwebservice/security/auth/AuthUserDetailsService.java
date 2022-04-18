688b5b5e1af84ba220434104247bb69db294f6f2
KarasTomasz <t.karas@onet.pl>
2022-04-18 21:14:56 +0200

    add appUserDetails; add exceptions; remove grantedAuthorities

--- src/main/java/pl/tkaras/carworkshopwebservice/security/auth/AuthUserDetailsService.java
+++ src/main/java/pl/tkaras/carworkshopwebservice/security/auth/AuthUserDetailsService.java
@@ -4,23 +4,33 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import pl.tkaras.carworkshopwebservice.exception.AppUserDetailsNotFoundException;
 import pl.tkaras.carworkshopwebservice.model.entity.AppUser;
+import pl.tkaras.carworkshopwebservice.model.entity.AppUserDetails;
 import pl.tkaras.carworkshopwebservice.repository.AppUserRepository;
+import pl.tkaras.carworkshopwebservice.repository.AppUserDetailsRepository;
 
 @Service
 public class AuthUserDetailsService implements UserDetailsService {
 
     private final AppUserRepository appUserRepository;
+    private final AppUserDetailsRepository userDetailsRepo;
 
-    public AuthUserDetailsService(AppUserRepository appUserRepository) {
+    public AuthUserDetailsService(AppUserRepository appUserRepository, AppUserDetailsRepository userDetailsRepo) {
         this.appUserRepository = appUserRepository;
+        this.userDetailsRepo = userDetailsRepo;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         AppUser appUser = appUserRepository
                 .findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException(String.format("Username %s not found", username)));
-        return new AuthUserDetails(appUser);
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        AppUserDetails userDetails = userDetailsRepo
+                .findByAppUserId(appUser.getId())
+                .orElseThrow(() -> new AppUserDetailsNotFoundException(username));
+
+        return new AuthUserDetails(appUser, userDetails);
     }
 }
