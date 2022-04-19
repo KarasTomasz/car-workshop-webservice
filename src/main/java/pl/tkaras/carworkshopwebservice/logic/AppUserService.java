e83ee6f04589a2c21d45acf0d0c891281e0c0a02
KarasTomasz <t.karas@onet.pl>
2022-04-19 12:44:53 +0200

    add AppUserDetailsDtoMapper, getUserById, getAllUsersDetails; remove getUserRole; refactor signUp, updateUser, deleteUser, enableAppUser

--- src/main/java/pl/tkaras/carworkshopwebservice/logic/AppUserService.java
+++ src/main/java/pl/tkaras/carworkshopwebservice/logic/AppUserService.java
@@ -1,13 +1,15 @@
 package pl.tkaras.carworkshopwebservice.logic;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import pl.tkaras.carworkshopwebservice.exception.AppUserDetailsNotFoundException;
+import pl.tkaras.carworkshopwebservice.exception.AppUserNotFoundException;
+import pl.tkaras.carworkshopwebservice.model.dto.AppUserDetailsDto;
 import pl.tkaras.carworkshopwebservice.model.dto.AppUserDto;
 import pl.tkaras.carworkshopwebservice.model.entity.AppUser;
 import pl.tkaras.carworkshopwebservice.model.entity.AppUserDetails;
 import pl.tkaras.carworkshopwebservice.model.entity.RegistrationConfirmToken;
+import pl.tkaras.carworkshopwebservice.model.mapper.impl.AppUserDetailsDtoMapper;
 import pl.tkaras.carworkshopwebservice.model.mapper.impl.AppUserDtoMapper;
 import pl.tkaras.carworkshopwebservice.repository.AppUserRepository;
 import pl.tkaras.carworkshopwebservice.repository.AppUserDetailsRepository;
@@ -24,44 +26,74 @@ public class AppUserService {
     private final AppUserRepository appUserRepo;
     private final AppUserDetailsRepository appUserDetailsRepo;
     private final AppUserDtoMapper appUserDtoMapper;
+    private final AppUserDetailsDtoMapper appUserDetailsDtoMapper;
     private final PasswordEncoder passwordEncoder;
     private final RegistrationConfirmTokenService registrationConfirmTokenService;
 
     public AppUserService(AppUserRepository appUserRepo,
-                          AppUserDetailsRepository appUserDetailsRepo, AppUserDtoMapper appUserDtoMapper, PasswordEncoder passwordEncoder, RegistrationConfirmTokenService registrationConfirmTokenService) {
+                          AppUserDetailsRepository appUserDetailsRepo,
+                          AppUserDtoMapper appUserDtoMapper,
+                          AppUserDetailsDtoMapper appUserDetailsDtoMapper,
+                          PasswordEncoder passwordEncoder,
+                          RegistrationConfirmTokenService registrationConfirmTokenService) {
         this.appUserRepo = appUserRepo;
         this.appUserDetailsRepo = appUserDetailsRepo;
         this.appUserDtoMapper = appUserDtoMapper;
+        this.appUserDetailsDtoMapper = appUserDetailsDtoMapper;
         this.passwordEncoder = passwordEncoder;
         this.registrationConfirmTokenService = registrationConfirmTokenService;
-    }
-
-    public AppUserDto getUserRole(String username){
-        if(appUserRepo.existsByUsername(username)) {
-            return appUserDtoMapper.mapToDto(appUserRepo.findByUsername(username)
-                    .orElseThrow(() -> new IllegalStateException(String.format("Problem with given username %s", username))));
-        }
-        throw new UsernameNotFoundException(String.format("User %s not found", username));
     }
 
     public List<AppUserDto> getAllUsers(){
         return appUserDtoMapper.mapToDtos(appUserRepo.findAll());
     }
 
-    public Optional<AppUserDto> getUser(String username){
-        AppUser user = appUserRepo.findByUsername(username)
-                .orElseThrow(() -> new IllegalStateException(String.format("User %s not found", username)));
+    public Optional<AppUserDto> getUserById(Long id){
+        AppUser user = appUserRepo.findById(id)
+                .orElseThrow(() -> new AppUserNotFoundException(id));
         AppUserDto userDto = appUserDtoMapper.mapToDto(user);
         return Optional.ofNullable(userDto);
     }
 
-    public String signUp(AppUser appUser){
+    public Optional<AppUserDto> getUser(String username){
+        AppUser user = appUserRepo.findByUsername(username)
+                .orElseThrow(() -> new AppUserNotFoundException(username));
+        AppUserDto userDto = appUserDtoMapper.mapToDto(user);
+        return Optional.ofNullable(userDto);
+    }
 
-        if(!appUserRepo.existsByUsername(appUser.getUsername())){
+    public List<AppUserDetailsDto> getAllUsersDetails(){
+        return appUserDetailsDtoMapper.mapToDtos(appUserDetailsRepo.findAll());
+    }
+
+    public Optional<AppUserDetailsDto> getUserDetail(String username){
+        Long id = appUserRepo.findByUsername(username)
+                .orElseThrow(() -> new AppUserNotFoundException(username))
+                .getId();
+
+        AppUserDetails userDetails = appUserDetailsRepo.findByAppUserId(id)
+                .orElseThrow(() -> new AppUserDetailsNotFoundException(username));
+
+        AppUserDetailsDto userDto = appUserDetailsDtoMapper.mapToDto(userDetails);
+        return Optional.ofNullable(userDto);
+    }
+
+    public String signUp(AppUserDto appUserdto){
+
+        if(!appUserRepo.existsByUsername(appUserdto.getUsername())){
+
+            AppUser appUser = new AppUser().builder()
+                    .username(appUserdto.getUsername())
+                    .password(passwordEncoder.encode(appUserdto.getPassword()))
+                    .email(appUserdto.getEmail())
+                    .firstname(appUserdto.getFirstname())
+                    .lastname(appUserdto.getLastname())
+                    .street(appUserdto.getStreet())
+                    .zipCode(appUserdto.getZipCode())
+                    .city(appUserdto.getCity())
+                    .build();
 
             String token = UUID.randomUUID().toString(); //generate register confirmation token
-
-            appUser.setPassword(passwordEncoder.encode(appUser.getPassword()));
 
             appUserRepo.save(appUser);
 
@@ -74,7 +106,7 @@ public class AppUserService {
             RegistrationConfirmToken confirmToken = RegistrationConfirmToken.builder()
                     .token(token)
                     .createdOn(LocalDateTime.now())
-                    .expiresOn(LocalDateTime.now().plusMinutes(20)) //the token will expire aftre 20 minutes
+                    .expiresOn(LocalDateTime.now().plusMinutes(20)) //the token will expire after 20 minutes
                     .appUser(appUser)
                     .build();
 
@@ -85,36 +117,33 @@ public class AppUserService {
         return null;
     }
 
-    public ResponseEntity updateUser(AppUser appUser){
-        if(appUserRepo.existsByUsername(appUser.getUsername())){
-            AppUser newAppUser = appUserRepo.findByUsername(appUser.getUsername())
-                    .orElseThrow(() -> new UsernameNotFoundException(String.format("Problem with given username %s", appUser.getUsername())));
-            appUserRepo.save(newAppUser);
-            return ResponseEntity.ok().build();
-        }
-        return ResponseEntity.notFound().build();
+    public AppUserDto updateUser(AppUser appUser){
+        AppUser newAppUser = appUserRepo.findByUsername(appUser.getUsername())
+                .orElseThrow(() -> new AppUserNotFoundException(appUser.getUsername()));
+        return appUserDtoMapper.mapToDto(appUserRepo.save(newAppUser));
     }
 
-    public ResponseEntity deleteUser(Long id){
+    public void deleteUser(Long id){
         if(appUserRepo.existsById(id)){
             appUserRepo.deleteById(id);
-            return ResponseEntity.ok().build();
         }
-        return ResponseEntity.notFound().build();
+        else {
+            throw new AppUserNotFoundException(id);
+        }
     }
 
     @Transactional
     public void enableAppUser(String username){
-        if(appUserRepo.existsByUsername(username)){
-            AppUser user = appUserRepo.findByUsername(username).orElseThrow();
-            AppUserDetails userDetails = appUserDetailsRepo.findByAppUserId(user.getId()).orElseThrow();
+        AppUser user = appUserRepo.findByUsername(username)
+                .orElseThrow(() -> new AppUserNotFoundException(username));
+        AppUserDetails userDetails = appUserDetailsRepo.findByAppUserId(user.getId())
+                .orElseThrow(() -> new AppUserDetailsNotFoundException(user.getId()));
 
-            userDetails.setAccountNonExpired(true);
-            userDetails.setAccountNonLocked(true);
-            userDetails.setCredentialsNonExpired(true);
-            userDetails.setEnabled(true);
-            appUserDetailsRepo.save(userDetails);
-        }
+        userDetails.setAccountNonExpired(true);
+        userDetails.setAccountNonLocked(true);
+        userDetails.setCredentialsNonExpired(true);
+        userDetails.setEnabled(true);
+        appUserDetailsRepo.save(userDetails);
     }
 
 }
