288ec6792aecf92734f381ac874a862368b5a114
KarasTomasz <t.karas@onet.pl>
2022-04-19 12:01:45 +0200

    add constructor with id

--- src/main/java/pl/tkaras/carworkshopwebservice/exception/AppUserNotFoundException.java
+++ src/main/java/pl/tkaras/carworkshopwebservice/exception/AppUserNotFoundException.java
@@ -9,4 +9,8 @@ public class AppUserNotFoundException extends RuntimeException {
     public AppUserNotFoundException(String username) {
         super(String.format("user %s not found", username));
     }
+
+    public AppUserNotFoundException(Long id) {
+        super(String.format("user with id %s not found", id));
+    }
 }
