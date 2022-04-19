e1eadd48ac349c0476e7a64b05bd15b82657a8ab
KarasTomasz <t.karas@onet.pl>
2022-04-19 02:59:04 +0200

    add findAll

--- src/main/java/pl/tkaras/carworkshopwebservice/repository/AppUserDetailsRepository.java
+++ src/main/java/pl/tkaras/carworkshopwebservice/repository/AppUserDetailsRepository.java
@@ -2,11 +2,13 @@ package pl.tkaras.carworkshopwebservice.repository;
 
 import pl.tkaras.carworkshopwebservice.model.entity.AppUserDetails;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface AppUserDetailsRepository {
 
+    List<AppUserDetails> findAll();
     Optional<AppUserDetails> findByAppUserId(Long id);
     AppUserDetails save(AppUserDetails appUserDetails);
     void deleteById(Long id);
