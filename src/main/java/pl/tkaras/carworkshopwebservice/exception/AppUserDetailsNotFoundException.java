642b60bccfe3c91ca748e8a22476b53cf046c8f0
KarasTomasz <t.karas@onet.pl>
2022-04-19 02:57:45 +0200

    add constructor with id

--- src/main/java/pl/tkaras/carworkshopwebservice/exception/AppUserDetailsNotFoundException.java
+++ src/main/java/pl/tkaras/carworkshopwebservice/exception/AppUserDetailsNotFoundException.java
@@ -9,4 +9,8 @@ public class AppUserDetailsNotFoundException extends RuntimeException {
     public AppUserDetailsNotFoundException(String username) {
         super(String.format("role and expiration information for user %s not found", username));
     }
+
+    public AppUserDetailsNotFoundException(Long id) {
+        super(String.format("role and expiration information for id %s not found", id));
+    }
 }
