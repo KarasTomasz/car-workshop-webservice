57f5a6ee1a89a686a3f36263d879967096cf8685
KarasTomasz <t.karas@onet.pl>
2022-04-19 02:49:51 +0200

    add Builder

--- src/main/java/pl/tkaras/carworkshopwebservice/model/entity/AppUser.java
+++ src/main/java/pl/tkaras/carworkshopwebservice/model/entity/AppUser.java
@@ -94,4 +94,72 @@ public class AppUser {
         this.city = city;
     }
 
+
+    public static Builder builder(){
+        return new Builder();
+    }
+
+    public static class Builder{
+        private String username;
+        private String password;
+        private String email;
+        private String firstname;
+        private String lastname;
+        private String street;
+        private String zipCode;
+        private String city;
+
+        public Builder username(String username){
+            this.username = username;
+            return this;
+        }
+
+        public Builder password(String password){
+            this.password = password;
+            return this;
+        }
+
+        public Builder email(String email){
+            this.email = email;
+            return this;
+        }
+
+        public Builder firstname(String firstname){
+            this.firstname = firstname;
+            return this;
+        }
+
+        public Builder lastname(String lastname){
+            this.lastname = lastname;
+            return this;
+        }
+
+        public Builder street(String street){
+            this.street = street;
+            return this;
+        }
+
+        public Builder zipCode(String zipCode){
+            this.zipCode = zipCode;
+            return this;
+        }
+
+        public Builder city(String city){
+            this.city = city;
+            return this;
+        }
+
+        public AppUser build(){
+            AppUser user = new AppUser();
+            user.username = username;
+            user.password = password;
+            user.email = email;
+            user.firstname = firstname;
+            user.lastname =lastname;
+            user.street = street;
+            user.zipCode = zipCode;
+            user.city = city;
+            return user;
+        }
+    }
 }
