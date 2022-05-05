08c2555fb0bee36f058ab516daddbac3a2178339
KarasTomasz <t.karas@onet.pl>
2022-05-06 01:57:57 +0200

    remove RegistrationConfirmTokenDtoMapper

--- src/main/java/pl/tkaras/carworkshopwebservice/services/RegistrationConfirmTokenService.java
+++ src/main/java/pl/tkaras/carworkshopwebservice/services/RegistrationConfirmTokenService.java
@@ -2,7 +2,6 @@ package pl.tkaras.carworkshopwebservice.services;
 
 import org.springframework.stereotype.Service;
 import pl.tkaras.carworkshopwebservice.models.entities.RegistrationConfirmToken;
-import pl.tkaras.carworkshopwebservice.models.mappers.impl.RegistrationConfirmTokenDtoMapper;
 import pl.tkaras.carworkshopwebservice.repositories.RegistrationConfirmTokenRepository;
 
 @Service
@@ -10,7 +9,7 @@ public class RegistrationConfirmTokenService {
 
     private final RegistrationConfirmTokenRepository confirmationTokenRepo;
 
-    public RegistrationConfirmTokenService(RegistrationConfirmTokenRepository confirmationTokenRepo, RegistrationConfirmTokenDtoMapper confirmTokenDtoMapper) {
+    public RegistrationConfirmTokenService(RegistrationConfirmTokenRepository confirmationTokenRepo) {
         this.confirmationTokenRepo = confirmationTokenRepo;
     }
 
