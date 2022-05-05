f926c1459727db4fb73b87877168a2abbd57951c
KarasTomasz <t.karas@onet.pl>
2022-05-06 01:52:22 +0200

    add appUserMapper, appUserDetailsMapper, confirmTokenMapper, refactor all methods

--- src/main/java/pl/tkaras/carworkshopwebservice/controllers/AppUserController.java
+++ src/main/java/pl/tkaras/carworkshopwebservice/controllers/AppUserController.java
@@ -4,12 +4,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import pl.tkaras.carworkshopwebservice.models.entities.AppUser;
+import pl.tkaras.carworkshopwebservice.models.entities.AppUserDetails;
+import pl.tkaras.carworkshopwebservice.models.mappers.impl.AppUserDetailsMapper;
+import pl.tkaras.carworkshopwebservice.models.mappers.impl.AppUserMapper;
+import pl.tkaras.carworkshopwebservice.models.mappers.impl.RegistrationConfirmTokenMapper;
 import pl.tkaras.carworkshopwebservice.services.AppUserService;
 import pl.tkaras.carworkshopwebservice.services.RegistrationService;
 import pl.tkaras.carworkshopwebservice.models.dtos.AppUserDetailsDto;
 import pl.tkaras.carworkshopwebservice.models.dtos.AppUserDto;
 import pl.tkaras.carworkshopwebservice.models.dtos.RegistrationConfirmTokenDto;
-import pl.tkaras.carworkshopwebservice.models.entities.AppUser;
 
 import java.util.List;
 
@@ -19,56 +23,67 @@ import java.util.List;
 public class AppUserController {
 
     private final AppUserService appUserService;
+
     private final RegistrationService registrationService;
 
-    public AppUserController(AppUserService appUserService, RegistrationService registrationService) {
+    private final AppUserMapper appUserMapper;
+
+    private final AppUserDetailsMapper appUserDetailsMapper;
+
+    private final RegistrationConfirmTokenMapper confirmTokenMapper;
+
+    public AppUserController(AppUserService appUserService, RegistrationService registrationService, AppUserMapper appUserMapper, AppUserDetailsMapper appUserDetailsMapper, RegistrationConfirmTokenMapper confirmTokenMapper) {
         this.appUserService = appUserService;
         this.registrationService = registrationService;
+        this.appUserMapper = appUserMapper;
+        this.appUserDetailsMapper = appUserDetailsMapper;
+        this.confirmTokenMapper = confirmTokenMapper;
     }
 
     @GetMapping("/all")
     @PreAuthorize("hasAuthority('user:readAll')")
     public List<AppUserDto> getAllUsers(){
-        return appUserService.getAllUsers();
+        return appUserMapper.mapToDtos(appUserService.getAllUsers());
     }
 
     @GetMapping("")
     @PreAuthorize("hasAuthority('user:read')")
     public ResponseEntity<AppUserDto> getAppUser(@RequestParam("username") String username){
-        return appUserService.getUser(username)
-                .map(response -> ResponseEntity.ok().body(response))
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        AppUser appUser = appUserService.getUser(username);
+        return new ResponseEntity<>(appUserMapper.mapToDto(appUser), HttpStatus.OK);
     }
 
     @GetMapping("/details/all")
     @PreAuthorize("hasAuthority('user:readAll')")
     public List<AppUserDetailsDto> getAllUsersDetails(){
-        return appUserService.getAllUsersDetails();
+        return appUserDetailsMapper.mapToDtos(appUserService.getAllUsersDetails());
     }
 
    @GetMapping("/details/")
     @PreAuthorize("hasAuthority('user:read')")
     public ResponseEntity<AppUserDetailsDto> getAppUserDetail(@RequestParam("username") String username){
-        return appUserService.getUserDetail(username)
-                .map(response -> ResponseEntity.ok().body(response))
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+       AppUserDetails appUserDetails = appUserService.getUserDetail(username);
+        return new ResponseEntity<>(appUserDetailsMapper.mapToDto(appUserDetails), HttpStatus.OK);
     }
 
     @PostMapping(path = "/confirm")
-    public ResponseEntity<Object> confirm(@RequestParam("token") String token) {
+    public ResponseEntity<?> confirm(@RequestParam("token") String token) {
         registrationService.confirm(token);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/registration")
     public ResponseEntity<RegistrationConfirmTokenDto> addAppUser(@RequestBody AppUserDto appUserdto){
-        return new ResponseEntity<>(registrationService.register(appUserdto), HttpStatus.CREATED);
+        AppUser appUser = appUserMapper.mapToEntity(appUserdto);
+        RegistrationConfirmTokenDto registrationConfirmTokenDto = confirmTokenMapper.mapToDto(registrationService.register(appUser));
+        return new ResponseEntity<>(registrationConfirmTokenDto, HttpStatus.CREATED);
     }
 
     @PutMapping("")
     @PreAuthorize("hasAuthority('user:update')")
-    public ResponseEntity<AppUserDto> updateAppUserRole(@RequestBody AppUser appUser){
-        return ResponseEntity.ok().body(appUserService.updateUser(appUser));
+    public ResponseEntity<AppUserDto> updateAppUserRole(@RequestBody AppUserDto appUserdto){
+        AppUser appUser = appUserService.updateUser(appUserMapper.mapToEntity(appUserdto));
+        return ResponseEntity.ok().body(appUserMapper.mapToDto(appUser));
     }
 
     @DeleteMapping("")
