8f5ac329172aed3c859481ac5a9bc113420766bd
KarasTomasz <t.karas@onet.pl>
2022-05-15 23:37:14 +0200

    add @RequiredArgsConstructor

--- src/main/java/pl/tkaras/carworkshopwebservice/services/RegistrationConfirmTokenService.java
+++ src/main/java/pl/tkaras/carworkshopwebservice/services/RegistrationConfirmTokenService.java
@@ -1,17 +1,15 @@
 package pl.tkaras.carworkshopwebservice.services;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.tkaras.carworkshopwebservice.models.entities.RegistrationConfirmToken;
 import pl.tkaras.carworkshopwebservice.repositories.RegistrationConfirmTokenRepository;
 
+@RequiredArgsConstructor
 @Service
 public class RegistrationConfirmTokenService {
 
     private final RegistrationConfirmTokenRepository confirmationTokenRepo;
-
-    public RegistrationConfirmTokenService(RegistrationConfirmTokenRepository confirmationTokenRepo) {
-        this.confirmationTokenRepo = confirmationTokenRepo;
-    }
 
     public void saveConfirmationToken(RegistrationConfirmToken confirmationToken){
         confirmationTokenRepo.save(confirmationToken);
