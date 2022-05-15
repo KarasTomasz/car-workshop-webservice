3a00211be540e533c13b63089c5674ee802b4baf
KarasTomasz <t.karas@onet.pl>
2022-05-15 23:37:40 +0200

    add @Repository

--- src/main/java/pl/tkaras/carworkshopwebservice/repositories/RegistrationConfirmTokenRepository.java
+++ src/main/java/pl/tkaras/carworkshopwebservice/repositories/RegistrationConfirmTokenRepository.java
@@ -1,11 +1,13 @@
 package pl.tkaras.carworkshopwebservice.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.tkaras.carworkshopwebservice.models.entities.RegistrationConfirmToken;
 
 import java.util.Optional;
 
-public interface RegistrationConfirmTokenRepository {
+@Repository
+public interface RegistrationConfirmTokenRepository extends JpaRepository<RegistrationConfirmToken, Long> {
 
     Optional<RegistrationConfirmToken> findByToken(String token);
-    RegistrationConfirmToken save(RegistrationConfirmToken confirmToken);
 }
