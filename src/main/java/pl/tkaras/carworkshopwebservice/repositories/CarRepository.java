85a1e9117486ec80935f0c583838de94275bb5b3
KarasTomasz <t.karas@onet.pl>
2022-05-15 23:42:18 +0200

    add @Repository; remove findById, findAll, existsById, save, deleteById

--- src/main/java/pl/tkaras/carworkshopwebservice/repositories/CarRepository.java
+++ src/main/java/pl/tkaras/carworkshopwebservice/repositories/CarRepository.java
@@ -1,22 +1,19 @@
 package pl.tkaras.carworkshopwebservice.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import pl.tkaras.carworkshopwebservice.models.entities.AppUser;
 import pl.tkaras.carworkshopwebservice.models.entities.Car;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface CarRepository {
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
 
-    Optional<Car> findById(Long id);
-    List<Car> findAll();
     List<Car> findAllByAppUserId(Long id);
 
     @Query("SELECT au FROM AppUser au WHERE au.username = ?1")
     Optional<AppUser> findByUsername(String username);
-    boolean existsById(Long id);
-    Car save(Car entity);
-    void deleteById(Long aLong);
-
 }
