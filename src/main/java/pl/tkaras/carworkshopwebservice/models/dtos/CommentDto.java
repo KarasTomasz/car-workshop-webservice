75dab6581266412ac88bfbc343683102623735d2
KarasTomasz <t.karas@onet.pl>
2022-05-16 23:39:31 +0200

    add @AllArgsConstructor, @NoArgsConstructor

--- src/main/java/pl/tkaras/carworkshopwebservice/models/dtos/CommentDto.java
+++ src/main/java/pl/tkaras/carworkshopwebservice/models/dtos/CommentDto.java
@@ -1,11 +1,15 @@
 package pl.tkaras.carworkshopwebservice.models.dtos;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Data
 public class CommentDto {
