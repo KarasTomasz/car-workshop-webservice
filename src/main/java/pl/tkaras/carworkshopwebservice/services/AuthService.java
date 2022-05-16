0a30c5f143a11555f83a45347564ea1f52fbf778
KarasTomasz <t.karas@onet.pl>
2022-05-16 23:34:16 +0200

    add PasswordEncoder, RegistrationConfirmTokenRepository, RegistrationConfirmTokenService, RegistrationConfirmTokenRepository, AppUserRepository; add signUp, enableAppUser

--- src/main/java/pl/tkaras/carworkshopwebservice/services/AuthService.java
+++ src/main/java/pl/tkaras/carworkshopwebservice/services/AuthService.java
@@ -5,16 +5,21 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import pl.tkaras.carworkshopwebservice.exceptions.AppUserNotFoundException;
 import pl.tkaras.carworkshopwebservice.exceptions.EmailSendingException;
 import pl.tkaras.carworkshopwebservice.exceptions.TokenNotFoundException;
 import pl.tkaras.carworkshopwebservice.models.entities.AppUser;
 import pl.tkaras.carworkshopwebservice.models.entities.RegistrationConfirmToken;
+import pl.tkaras.carworkshopwebservice.repositories.AppUserRepository;
 import pl.tkaras.carworkshopwebservice.repositories.RegistrationConfirmTokenRepository;
 import pl.tkaras.carworkshopwebservice.securities.jwt.JwtUtils;
 
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
@@ -24,11 +29,24 @@ public class AuthService {
 
     private final AuthenticationManager authenticationManager;
 
-    private final RegistrationConfirmTokenRepository confirmTokenRepo;
-
-    private final AppUserService appUserService;
+    private final PasswordEncoder passwordEncoder;
 
     private final EmailSenderService emailSenderService;
+
+    private final RegistrationConfirmTokenService registrationConfirmTokenService;
+
+    private final RegistrationConfirmTokenRepository confirmTokenRepo;
+
+    private final AppUserRepository appUserRepo;
+
+    public List<AppUser> getAllUsersDetails(){
+        return appUserRepo.findAll();
+    }
+
+    public AppUser getAppUser(String username){
+        return appUserRepo.findByUsername(username)
+                .orElseThrow(() -> new AppUserNotFoundException(username));
+    }
 
     public String login(String username, String password){
         Authentication authentication = authenticationManager.authenticate(
@@ -40,10 +58,10 @@ public class AuthService {
         return (jwtUtils.getTokenPrefix() + jwtToken);
     }
 
-    //@Transactional
+    @Transactional
     public RegistrationConfirmToken register(AppUser appUser) {
 
-        String token = appUserService.signUp(appUser);
+        String token = signUp(appUser);
 
         if(token != null){
             try {
@@ -59,6 +77,30 @@ public class AuthService {
         return null;
     }
 
+    public String signUp(AppUser appUser){
+
+        if(!appUserRepo.existsByUsername(appUser.getUsername())){
+
+            appUser.setPassword(passwordEncoder.encode(appUser.getPassword()));
+
+            String token = UUID.randomUUID().toString(); //generate register confirmation token
+
+            appUserRepo.save(appUser);
+
+            RegistrationConfirmToken confirmToken = RegistrationConfirmToken.builder()
+                    .token(token)
+                    .createdOn(LocalDateTime.now())
+                    .expiresOn(LocalDateTime.now().plusMinutes(20)) //the token will expire after 20 minutes
+                    .appUser(appUser)
+                    .build();
+
+            registrationConfirmTokenService.saveConfirmationToken(confirmToken);
+
+            return token;
+        }
+        return null;
+    }
+
     @Transactional
     public void confirm(String token){
         RegistrationConfirmToken registrationConfirmToken = confirmTokenRepo.findByToken(token)
@@ -66,7 +108,7 @@ public class AuthService {
 
         registrationConfirmToken.setConfirmedOn(LocalDateTime.now());
 
-        appUserService.enableAppUser(registrationConfirmToken.getAppUser().getUsername());
+        enableAppUser(registrationConfirmToken.getAppUser().getUsername());
 
         AppUser user = registrationConfirmToken.getAppUser();
         registrationConfirmToken.setAppUser(user);
@@ -81,5 +123,16 @@ public class AuthService {
         content = content.replace("[[name]]", name);
         content = content.replace("[[URL]]", link);
         return content;
+    }
+
+    public void enableAppUser(String username){
+        AppUser foundUser = appUserRepo.findByUsername(username)
+                .orElseThrow(() -> new AppUserNotFoundException(username));
+
+        foundUser.setAccountNonExpired(true);
+        foundUser.setAccountNonLocked(true);
+        foundUser.setCredentialsNonExpired(true);
+        foundUser.setEnabled(true);
+        appUserRepo.save(foundUser);
     }
 }
