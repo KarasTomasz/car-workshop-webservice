f0b9b8a4a36539da30fdd45b521b1b56f97d97b5
KarasTomasz <t.karas@onet.pl>
2022-05-16 23:40:23 +0200

    add @AllArgsConstructor, @NoArgsConstructor

--- src/main/java/pl/tkaras/carworkshopwebservice/models/dtos/RegistrationConfirmTokenDto.java
+++ src/main/java/pl/tkaras/carworkshopwebservice/models/dtos/RegistrationConfirmTokenDto.java
@@ -1,10 +1,14 @@
 package pl.tkaras.carworkshopwebservice.models.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Data
 public class RegistrationConfirmTokenDto {
