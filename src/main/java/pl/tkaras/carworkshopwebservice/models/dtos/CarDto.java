d1f1ae9605ab3aefa45aff63cff12d99a0f2bc99
KarasTomasz <t.karas@onet.pl>
2022-05-16 23:37:30 +0200

    add @AllArgsConstructor, @NoArgsConstructor

--- src/main/java/pl/tkaras/carworkshopwebservice/models/dtos/CarDto.java
+++ src/main/java/pl/tkaras/carworkshopwebservice/models/dtos/CarDto.java
@@ -1,9 +1,13 @@
 package pl.tkaras.carworkshopwebservice.models.dtos;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Data
 public class CarDto {
