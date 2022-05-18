d77eca0c641bfefcb2673fe1f6ab0a2e446a9a0c
KarasTomasz <t.karas@onet.pl>
2022-05-18 19:25:47 +0200

    rename from AppUserAddressNotFound to AppUserAddressNotFoundException

--- src/main/java/pl/tkaras/carworkshopwebservice/exceptions/AppUserAddressNotFoundException.java
+++ src/main/java/pl/tkaras/carworkshopwebservice/exceptions/AppUserAddressNotFoundException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class AppUserAddressNotFound extends RuntimeException{
+public class AppUserAddressNotFoundException extends RuntimeException{
 
-    public AppUserAddressNotFound(Long id) {
+    public AppUserAddressNotFoundException(Long id) {
         super("AppUserAddress with the given id " + id + " was not found");
     }
 }
